0c63d72aca5b4a43fba65842ef736e5aad62a599
snowm <ridge.arai@gmail.com>
2020-10-17 20:02:56 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedSquareAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedSquareAuto.java
@@ -20,15 +20,18 @@ public class RedSquareAuto extends AutonomousControl {
 
         if (opModeIsActive()){
             // left square
+            rob.encoderMovement(0.5, 1, 10, 1000, Goal.movements.clawIn, rob.claw);
             rob.driveTrainEncoderMovement(0.2, 12, 5, 1000, Goal.movements.left);
             rob.driveTrainEncoderMovement(0.2, 80, 5, 50000, Goal.movements.forward);        }
 
             // low right square
-            rob.driveTrainEncoderMovement(0.2, 6, 5, 1000, Goal.movements.right);
+        rob.encoderMovement(0.5, 1, 10, 1000, Goal.movements.clawIn, rob.claw);
+        rob.driveTrainEncoderMovement(0.2, 6, 5, 1000, Goal.movements.right);
             rob.driveTrainEncoderMovement(0.2, 60, 5, 50000, Goal.movements.forward);
 
             // low right square
-            rob.driveTrainEncoderMovement(0.2, 6, 5, 1000, Goal.movements.right);
+        rob.encoderMovement(0.5, 1, 10, 1000, Goal.movements.clawIn, rob.claw);
+        rob.driveTrainEncoderMovement(0.2, 6, 5, 1000, Goal.movements.right);
             rob.driveTrainEncoderMovement(0.2, 90, 5, 50000, Goal.movements.forward);
 
     }
